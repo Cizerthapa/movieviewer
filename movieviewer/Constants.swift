c8167671722f620646d578acd790de7d25000d19
Cizer Thapa <cizerthapa@gmail.com>
2025-12-13 16:53:18 +0545

    feat: added the extension func for reusable style and added button in home

--- movieviewer/Constants.swift
+++ movieviewer/Constants.swift
@@ -1,13 +1,30 @@
 import Foundation
+import SwiftUI
 
 struct Constants{
-    static let homeStrings = "Home";
-    static let upcomingStrings = "Upcoming";
-    static let searchStrings = "Search";
-    static let downloadStrings = "Download";
+    static let homeStrings = "Home"
+    static let upcomingStrings = "Upcoming"
+    static let searchStrings = "Search"
+    static let downloadStrings = "Download"
+    static let playString = "Play"
     
-    static let homeIconString = "house";
-    static let upcomingIconString = "play.circle";
-    static let magnifyingglassIconString = "magnifyingglass";
-    static let downArrowIconString = "arrow.down.to.line";
+    static let homeIconString = "house"
+    static let upcomingIconString = "play.circle"
+    static let magnifyingglassIconString = "magnifyingglass"
+    static let downArrowIconString = "arrow.down.to.line"
+    
+    static let testTitleURL = "https://assets-prd.ignimgs.com/2025/09/30/zootopia2-button-replacement-1759237213360.jpg"
+}
+
+extension Text {
+    func ghostButtom() -> some View {
+        self
+            .frame(width: 100, height: 50)
+            .foregroundColor(.buttonText)
+            .bold()
+            .background{
+                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .stroke(.buttonBorder,lineWidth: 5)
+            }
+    }
 }
---
+++ movieviewer/HomeView.swift
@@ -0,0 +1,42 @@
+//
+//  HomeView.swift
+//  movieviewer
+//
+//  Created by Cizer on 12/13/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var heroTestTitle = Constants.testTitleURL
+    
+    var body: some View {
+        VStack{
+            AsyncImage(url: URL(string: heroTestTitle)){ image in
+                image
+                    .resizable()
+                    .scaledToFit()
+            } placeholder: {
+                ProgressView()
+            }}
+        
+        HStack{
+            Button{
+                
+            } label: {
+                Text(Constants.playString)
+                    .ghostButtom()
+            }
+            Button{
+                
+            } label: {
+                Text(Constants.downloadStrings)
+                    .ghostButtom()
+            }
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
