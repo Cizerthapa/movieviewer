612228b3e4f4e7fecf9187235567181ccf61b7e4
Cizer Thapa <cizerthapa@gmail.com>
2025-12-22 11:38:34 +0545

    chore: new url added and work of layout for list view

--- movieviewer/Constants.swift
+++ movieviewer/Constants.swift
@@ -7,13 +7,16 @@ struct Constants{
     static let searchStrings = "Search"
     static let downloadStrings = "Download"
     static let playString = "Play"
+    static let trendingMovieString = "Trending Movies"
     
     static let homeIconString = "house"
     static let upcomingIconString = "play.circle"
     static let magnifyingglassIconString = "magnifyingglass"
     static let downArrowIconString = "arrow.down.to.line"
     
-    static let testTitleURL = "https://assets-prd.ignimgs.com/2025/09/30/zootopia2-button-replacement-1759237213360.jpg"
+    static let testTitleURL = "https://static.wikia.nocookie.net/movienowbox197189/images/7/74/Zootopia-1.jpg/revision/latest?cb=20181106142235"
+    static let testTitleURL2 = "https://m.media-amazon.com/images/M/MV5BZTljOGJiNjYtYTY1Ni00Yzk4LWI2YTUtNTdhYmYyMTgyNDMxXkEyXkFqcGc@._V1_.jpg"
+    static let testTitleURL3 = "https://static.wikia.nocookie.net/greatestmovies/images/7/78/Madagascar.jpeg/revision/latest?cb=20230809194242"
 }
 
 extension Text {
--- movieviewer/HorizontalListView.swift
+++ movieviewer/HorizontalListView.swift
@@ -1,8 +1,34 @@
-//
-//  HorizontalListView.swift
-//  movieviewer
-//
-//  Created by Cizer on 12/14/25.
-//
+import SwiftUI
 
-import Foundation
+struct HorizontalListView: View {
+    let header : String
+    var titles = [Constants.testTitleURL, Constants.testTitleURL2, Constants.testTitleURL3]
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(header)
+                .font(.title)
+            
+            ScrollView(.horizontal) {
+                LazyHStack {
+                    ForEach(titles, id: \.self) { title in
+                        AsyncImage(url: URL(string: title)) { image in
+                            image
+                                .resizable()
+                                .scaledToFit()
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 120,height: 200)
+                    }
+                }
+            }
+        }.frame(height: 250)
+            .padding(10)
+    }
+}
+
+#Preview {
+    HorizontalListView(header: Constants.trendingMovieString)
+}
